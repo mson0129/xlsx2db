9e6bcbb9b74e2502f358a79f251eb22df694719f
Michael Son <mson0129@gmail.com>
2023-02-28 14:41:07 +0900

    Bug fix & README updated

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# xlsx2db
+# XLSX2DB
+
+[![MIT License](https://img.shields.io/github/license/mson0129/xlsx2db)](https://www.mit.edu/~amini/LICENSE.md)
+![Repo Size](https://img.shields.io/github/repo-size/mson0129/xlsx2db)
+![Last Commit](https://img.shields.io/github/last-commit/mson0129/xlsx2db)
+![Release Version](https://img.shields.io/github/v/release/mson0129/xlsx2db)
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fmson0129%2Fxlsx2db&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+
+XLSX 파일을 읽어 DB 테이블에 입력합니다.
+MSSQL, MySQL, MariaDB를 지원합니다.
+
+## 사용법
+
+### XLSX 파일 준비
+
+DB에 입력할 데이터가 있는 파일입니다.
+첫 번째 행에는 컬럼명을 입력하고, 나머지 행에는 데이터를 입력합니다.
+example.xlsx를 복사 후 수정하여 작성하시면 편리합니다.
+
+
+### xlsx2db.ini 파일 준비
+
+DB 접속정보를 입력하는 파일입니다.
+
+여러 개의 DB를 사용하고자 하는 경우 ini 파일에 섹션을 여러개 추가하여 사용하셔도 됩니다.
+```
+[섹션 이름]
+dbms_name = DBMS의 이름입니다. mssql | mysql | mariadb 중 하나의 값을 사용합니다. 대소문자 구분하지 않습니다.
+dbms_server = DBMS 서버의 주소입니다. 도메인 형식, IP주소 형식 모두 가능합니다.
+dbms_username = DBMS 사용자 이름입니다.
+dbms_password = DBMS 사용자의 암호입니다.
+database_name = 기본 데이터 베이스 이름입니다.
+```
+
+### 실행
+새로운 파일을 생성하여 아래와 같이 코드 작성후 사용할 수 있습니다.
+```
+from xlsx2db import XLSX2DB
+
+xlsx2db = XLSX2DB(section="DEFAULT") # section은 설정 파일(xlsx2db.ini)의 섹션명
+output: bool = xlsx2db.convert_xlsx2db(path="example.xlsx", table="example") # path는 엑셀 파일의 경로, table은 DB 테이블의 이름
+print(output) # True인 경우 정상 종료
+```
+
+위의 코드 작성이 어려운 경우 xlsx2db.py의 하단 코드를 수정하여 사용하여도 됩니다.
--- xlsx2db.py
+++ xlsx2db.py
@@ -1,6 +1,7 @@
 """
 # XLSX2DB
 XLSX 파일을 DB 테이블에 입력합니다.
+MSSQL, MySQL, MariaDB를 지원합니다.
 
 ## Contributor
 - 손남규(snk@fasoo.com)
@@ -15,6 +16,7 @@ pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
 # 외부 모듈
 import openpyxl
 import pymssql
+import pymysql
 
 class XLSX2DB:
     """
@@ -27,25 +29,25 @@ class XLSX2DB:
     dbms_username: str                      # DBMS 사용자 이름
     dbms_password: str                      # DBMS 사용자 암호
     database_name: str                      # 데이터베이스 이름
-    table_name: str                         # 테이블 이름
 
-    def __init__(self) -> None:
+
+    def __init__(self, section: str = "DEFAULT") -> None:
         config = configparser.ConfigParser()
         config.read("xlsx2db.ini")
-        self.dbms_name = config["DEFAULT"]["dbms_name"]
-        self.dbms_server = config["DEFAULT"]["dbms_server"]
-        self.dbms_username = config["DEFAULT"]["dbms_username"]
-        self.dbms_password = config["DEFAULT"]["dbms_password"]
-        self.database_name = config["DEFAULT"]["database_name"]
-        self.table_name = config["DEFAULT"]["table_name"]
+        self.dbms_name = config[section]["dbms_name"]
+        self.dbms_server = config[section]["dbms_server"]
+        self.dbms_username = config[section]["dbms_username"]
+        self.dbms_password = config[section]["dbms_password"]
+        self.database_name = config[section]["database_name"]
 
-    def convert_xlsx2db(self, path: str) -> bool:
+
+    def convert_xlsx2db(self, path: str, table: str) -> bool:
         """
         # convert_xlsx2db()
         XLSX 파일을 읽어, 1행에 있는 컬럼명을 기준으로 DB에 데이터를 삽입합니다.
 
         ## Args:
-        1. path: str = "example.xlsx"                       # 엑셀 파일 경로.
+        1. path: str = "example.xlsx"
 
         ## Returns:
         output: bool = True # 정상실행일 경우 True
@@ -53,6 +55,27 @@ class XLSX2DB:
         output: bool = True
 
         # Part I. Loading Excel File
+        column_names, records = self.load_xlsx(path)
+
+        # Part II. Create Query
+        query = self.create_query(table, column_names)
+
+        # Part III. Execute Query
+        return self.execute_query(query, records)
+
+
+    # Part I. Loading Excel XLSX File
+    def load_xlsx(self, path: str) -> list:
+        """
+        # load_xlsx()
+        XLSX 파일을 읽어, 1행에 있는 컬럼명을 기준으로 DB에 삽입하기 위한 리스트를 생성합니다.
+
+        ## Args:
+        1. path: str = "example.xlsx"
+
+        ## Returns:
+        output: tuple = (column_names, records)
+        """
         workbook = openpyxl.load_workbook(path)
         worksheet = workbook.active
 
@@ -65,40 +88,77 @@ class XLSX2DB:
                 column_names = list(row)
             else:
                 # Values of record(레코드값)
-                records.append(tuple([list(row)[i] for i in range(len(column_names)) if column_names[i] is not None]))
+                # records.append(dict(zip(column_names, list(row))))
+                records.append(list(row))
             i += 1
         print("{dt}: End of loading the file...".format(dt = datetime.datetime.now().strftime("%Y-%m-%d T %H:%M:%S")))
 
         if self.__debug:
             print("column_names = ")
             pp.pprint(column_names)
-            print("records = ")
+            print("output = ")
             pp.pprint(records)
 
-        # Part II. Execute Query
+        return (column_names, records)
+
+
+    # Part II. Create Query
+    def create_query(self, table: str, column_names: list) -> str:
+        """
+        # create_query()
+        쿼리를 생성합니다.
+
+        ## Args:
+        1. table: str = "example"
+        2. column_names: list = ["column1", ...]
+
+        ## Returns:
+        output: str = "INSERT INTO example (column1, ...) VALUES ('value1', ...); ..."
+        """
+        columns = ", ".join(column_names)
+        values_placeholder = ", ".join(["%s"]*len([column_name for column_name in column_names if column_name is not None]))
+        output = f"""INSERT INTO {table} ({columns}) VALUES ({values_placeholder})"""
+
+        if self.__debug:
+            print("query = ")
+            print(output)
+
+        return output
+    
+
+    # Part III. Execute Query
+    def execute_query(self, query: str, records: list) -> bool:
+        """
+        # execute_query()
+        쿼리를 여러번 실행합니다.
+
+        ## Args:
+        1. query: str = "INSERT INTO example (column1, ...) VALUES ('value1', ...); ..."
+        2. records: list = [("value1", ...), ...]
+
+        ## Returns:
+        output: bool = True # 정상실행일 경우 True
+        """
+        output: bool = True
+
         try:
             if self.dbms_name.lower() == "mssql":
                 connection = pymssql.connect(server = self.dbms_server, user = self.dbms_username, password = self.dbms_password, database = self.database_name)
-                cursor = connection.cursor()
-                
-                columns = ", ".join(column_names)
-                values_placeholder = ", ".join(["%s"]*len([column_name for column_name in column_names if column_name is not None]))
-                query = f"""INSERT INTO {self.table_name} ({columns}) VALUES ({values_placeholder})"""
-
-                if self.__debug:
-                    print("query = ")
-                    print(query)
-
-                cursor.executemany(query, records)
-                connection.commit()
-                connection.close()
+            elif self.dbms_name.lower() == "mysql" or self.dbms_name.lower() == "mariadb":
+                connection = pymysql.connect(host = self.dbms_server, user = self.dbms_username, password = self.dbms_password, db = self.database_name, charset = "utf8")
+            cursor = connection.cursor()
+            cursor.executemany(query, records)
+            connection.commit()
+            connection.close()
         except Exception as e:
-            print("Database Error: ", e)
+            if self.__debug:
+                print("Database Error: ", e)
             output = False
 
         return output
 
 
+    # Methods for manual run
     def convert_xlsx2dictlist(self, path: str) -> list:
         """
         # convert_xlsx2list()
@@ -130,7 +190,7 @@ class XLSX2DB:
         return output
 
 
-    def convert_dictlist2sql(self, data: list) -> str:
+    def convert_dictlist2sql(self, table: str, data: list) -> str:
         """
         # convert_dictlist2sql()
 
@@ -148,14 +208,14 @@ class XLSX2DB:
 
             pp.pprint(values)
 
-            output += f"INSERT INTO {self.table_name} ({columns}) VALUES ({values});\n"
+            output += f"INSERT INTO {table} ({columns}) VALUES ({values});\n"
 
         return output
 
 
-    def execute_query(self, query: str) -> bool:
+    def execute(self, query: str) -> bool:
         """
-        # execute_query()
+        # execute()
         쿼리를 실행합니다.
 
         ## Args:
@@ -168,11 +228,13 @@ class XLSX2DB:
 
         try:
             if self.dbms_name.lower() == "mssql":
-                connection = pymssql.connect(server = self.dbms_server, user = self.dbms_username, password = self.dbms_password, database = self.database_name)
-                cursor = connection.cursor()
-                cursor.execute(query)
-                connection.commit()
-                connection.close()
+                connection = pymssql.connect(server = self.dbms_server, user = self.dbms_username, password = self.dbms_password, database = self.database_name, charset = "utf8")
+            elif self.dbms_name.lower() == "mysql" or self.dbms_name.lower() == "mariadb":
+                connection = pymysql.connect(host = self.dbms_server, user = self.dbms_username, password = self.dbms_password, db = self.database_name, charset = "utf8")
+            cursor = connection.cursor()
+            cursor.execute(query)
+            connection.commit()
+            connection.close()
         except:
             output = False
 
@@ -180,7 +242,6 @@ class XLSX2DB:
 
 
 if __name__ == "__main__":
-    xlsx2db = XLSX2DB()
-    path: str = "example.xlsx"
-    output: bool = xlsx2db.convert_xlsx2db(path=path)
-    print(output)
+    xlsx2db = XLSX2DB(section="DEFAULT") # section은 설정 파일(xlsx2db.ini)의 섹션명
+    output: bool = xlsx2db.convert_xlsx2db(path="example.xlsx", table="example") # path는 엑셀 파일의 경로, table은 DB 테이블의 이름
+    print(output) # True인 경우 정상 종료
