b7009f143038c29e4d7d7d66163f0906c5e333ab
Michael Son <mson0129@gmail.com>
2024-05-20 12:32:59 +0900

    Create util.py

---
+++ util.py
@@ -0,0 +1,16 @@
+class bcolors:
+    """
+    https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
+    """
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    def colored(r, g, b, text):
+        return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
